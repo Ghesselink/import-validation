2cc6695748b3f1129cb0ec3dca5cd0194eff1a96
Ghesselink <geert.hess@gmail.com>
2023-12-19 12:42:30 +0100

    setup nested components

--- ifc-import-comparison.py
+++ ifc-import-comparison.py
@@ -5,165 +5,234 @@ import ifcopenshell
 from ifcopenshell.file import file as IfcFile
 from ifcopenshell.entity_instance import entity_instance as IfcEntity
 
-def do_try(fn, default: Optional[Any] = None) -> Any:
-    try:
-        return fn()
-    except:
-        import traceback
-        traceback.print_exc()
-        return default
+
+class Component(ABC):
+    def __init__(self, entity_instance, name, parent = None):
+        self.entity_instance = entity_instance
+        self.name = name
+        self.parent = parent
+        self.entity_name = self.calculate_entity_name()
+        self.guid = self.calculate_guid()
+        
+    def calculate_entity_name(self):
+        # Logic to extract name from entity
+        return self.entity_instance.Name if hasattr(self.entity_instance, 'Name') else None
+
+    def calculate_guid(self):
+        # Logic to extract GUID from entity
+        return self.entity_instance.GlobalId if hasattr(self.entity_instance, 'GlobalId') else None
+    
+    def get_parent(self):
+        return self.parent
+
+    def display(self):
+        print(f"{self.__class__.__name__}: {self.entity_instance} ({self.guid})")
+    
+    def check_import(self, other, report):
+        if self.guid != other.guid:
+            report.add_discrepancy(f"Different GUID: {self.guid} vs {other.guid}")
+        if self.name != other.name:
+            report.add_discrepancy(f"Different name: {self.name} vs {other.name}")
+
+class Project(Component):
+    def __init__(self, entity_instance, name):
+        super().__init__(entity_instance, name)
+        self.sites = []
+        self._init_sites()
+
+    def _init_sites(self):
+            decomposes = self.entity_instance.IsDecomposedBy or []
+            for related_object in decomposes.RelatedObjects:
+                if related_object.is_a('IfcSite'):
+                    self.sites.append(Site(entity_instance = related_object,
+                                        name = 'Site', 
+                                        parent=self))
 
 
-class IfcContext(ABC):
-    """
-    Abstract class for IFC context; common attributes and methods
-    """
-    def __init__(self, file_path: str):
-        self.file: IfcFile = ifcopenshell.open(file_path)
-        self.entities: List[IfcEntity] = []
-        self.entity_types: Set[str] = set()
-        self.entities_with_props: Dict[str, Dict[str, Any]] = {}
-        self.calculate_entities()
+class Site(Component):
+    def __init__(self, entity_instance, name):
+        super().__init__(entity_instance, name)
+        self.buildings = []
+        self._init_buildings()
 
-    @abstractmethod
-    def calculate_entities(self) -> None:
-        pass
+    def _init_buildings(self):
+        decomposes = self.entity_instance.IsDecomposedBy or []
+        for decomposition in decomposes:
+            for related_object in decomposition.RelatedObjects:
+                if related_object.is_a('IfcBuilding'):
+                    self.buildings.append(Building(entity_instance = related_object,
+                                                   name = 'Building', 
+                                                   parent=self))
 
-    def get_creation_date(self) -> str:
-        owner_history = self.file.by_type('IfcOwnerHistory')
-        if owner_history:
-            time_stamp = owner_history[0].CreationDate
-            return datetime.utcfromtimestamp(time_stamp).strftime('%Y-%m-%d %H:%M:%S')
-        return 'No creation date'
-
-    def get_iso_timestamp(self) -> str:
-        return self.file.wrapped_data.header.file_name.time_stamp
-
-    def add_property_set(self, entity: IfcEntity) -> None:
-        prop_set = self.get_property_set(entity)
-        if prop_set:
-            self.entities_with_props[entity.GlobalId] = {
-                'type': entity.is_a(),
-                'property_set': prop_set
-            }
-
-    def get_property_set(self, entity: IfcEntity) -> Optional[Any]:
-        if hasattr(entity, 'IsDefinedBy') and entity.IsDefinedBy:
-            for definition in entity.IsDefinedBy:
-                if definition.is_a('IfcRelDefinesByProperties'):
-                    property_set = definition.RelatingPropertyDefinition
-                    if property_set and property_set.is_a('IfcPropertySet'):
-                        return property_set
-            return None
+    def display(self):
+        print(f'Site: {self.entity_instance} ({self.guid})')
+        for building in self.buildings:
+            building.display()
+        
+    def check_import(self, other, report):
+        super().check_import(other, report)
+        if len(self.buildings) != len(other.buildings):
+            report.add_discrepancy(f"Different number of buildings in site {self.guid}")
+        for self_building, other_building in zip(self.buildings, other.buildings):
+            if self_building.guid != other_building.guid or self_building.name != other_building.name:
+                report.add_discrepancy(f"Mismatch in building details for site {self.guid}")
+            self_building.check_import(other_building, report)
 
 
-class IfcOriginalContext(IfcContext):
-    def calculate_entities(self):
-        for entity in self.file:
-            self.entities.append(entity)
-            self.add_property_set(entity)
-            self.entity_types.add(entity.is_a())
+class Building(Component):
+    def __init__(self, entity_instance, name, parent = None):
+        super().__init__(entity_instance, name, parent = None)
+        self.storeys = []
+        self._init_storeys()
+
+    def _init_storeys(self):
+        decomposes = self.entity_instance.IsDecomposedBy or []
+        for decomposes in decomposes:
+            for related_object in decomposes.RelatedObjects:
+                if related_object.is_a('IfcBuildingStorey'):
+                    self.storeys.append(Storey(entity_instance = related_object,
+                                               name = related_object.Name, parent=self))
+
+    def display(self):
+        print(f'Building: {self.entity_instance} ({self.guid}')
+        for storeys in self.storeys:
+            storeys.display()
+        
+    def get_storey_names(self):
+        return [storey.name for storey in self.storeys]
+
+    def check_import(self, other, report):
+        super().check_import(other, report)
+        for storey, other_storey in zip(self.storeys, other.storeys):
+             storey.check_import(other_storey, report)
+
+        if len(self.storeys) != len(other.storeys):
+            report.add_discrepancy(f"Different number of storeys in building {self.guid}")
+
+        for self_storey, other_storey in zip(self.storeys, other.storeys):
+            if self_storey.guid != other_storey.guid or self_storey.name != other_storey.name:
+                report.add_discrepancy(f"Mismatch in storey details for building {self.guid}")
+            self_storey.check_import(other_storey, report)
 
 
-class IfcImportContext(IfcContext):
-    def __init__(self, file_path:str, ifc_original_context : IfcOriginalContext):
-        self.ifc_original_context = ifc_original_context
-        self.report: Dict[str, Dict[str, Any]] = {}
-        super().__init__(file_path)
+class Storey(Component):
+    def __init__(self, entity_instance, name, parent = None):
+        super().__init__(entity_instance, name, parent = None)
+        self.components = {
+            'IfcBeam': [],
+            'IfcColumn': [],
+            'IfcCovering': [],
+            'IfcDistributionElement': [],
+            'IfcDoor': [],
+            'IfcMember': [],
+            'IfcObject': [],
+            'IfcOpening': [],
+            'IfcPipe': [],
+            'IfcRailing': [],
+            'IfcRoof': [],
+            'IfcSlab': [],
+            'IfcSpace': [],
+            'IfcStair': [],
+            'IfcWall': [],
+            'IfcWindow': []
+        }
+        self._init_storey_elements()
 
-    def calculate_entities(self):
-        for t in self.ifc_original_context.entity_types:
-            insts = self.file.by_type(t)
-            if not insts:
-                self.report[t] = {'expected': f'entity of type {t}', 'observed': None}
-            else:
-                self.entities.extend(insts)
-                for inst in insts:
-                    self.add_property_set(inst)
-                self.entity_types.add(t)
+    def _init_storey_elements(self):
+        contains_elements = self.entity_instance.ContainsElements or []
+        for contains_element in contains_elements:
+            for related_element in contains_element.RelatedElements:
+                entity_type = related_element.is_a()
+                component_class = globals().get(entity_type, None)
+                if component_class and entity_type in self.components:
+                    component = component_class(entity_instance=related_element,
+                                                name=related_element.Name, 
+                                                parent=self)
+                    self.components[entity_type].append(component)
+
+    def display(self):
+        print(f"Storey: {self.entity_instance} ({self.guid}")
 
 
-def compare_entity_counts(ifc_original_context : IfcOriginalContext, ifc_import_context: IfcImportContext) -> None:
-    """
-    Compare entity counts between original and import context
-    Modifies ifc_import_context.report
-    """
-    for t in ifc_import_context.entity_types:
-        original_count = len(ifc_original_context.file.by_type(t))
-        import_count = len(ifc_import_context.file.by_type(t))
-        if original_count != import_count:
-            ifc_import_context.report[t] = {'expected': f'{original_count} num of instances', 'observed': import_count}
-
-def compare_entity_guids(ifc_original_context, ifc_import_context):
-    """
-    @todo restructure guidIds. Probably set context.entities to a dict with what as key?
-    """
-    # guid_ids_import = [entity.GlobalId for entity in entities_import]
-    # guid_ids_export = [entity.GlobalId for entity in entities_export]
-
-    # missing_guids = [guid for guid in guid_ids_export if guid not in guid_ids_import]
-    # if len(missing_guids) > 0:
-    #     print(f'guids not present in import: {len(missing_guids)}')
+class IfcBeam(Component):
     pass
 
-def compare_entity_timestamps(ifc_original_context: IfcOriginalContext, ifc_import_context: IfcImportContext) -> None:
-    """
-    Compare the creation dates and ISO timestamps between original and import contexts.
-    Adds the timestamps to the respective context instances.
-    """
-    # Calculate and set timestamps for original context
-    ifc_original_context.creation_date = ifc_original_context.get_creation_date()
-    ifc_original_context.iso_timestamp = ifc_original_context.get_iso_timestamp()
+class IfcColumn(Component):
+    pass
 
-    # Calculate and set timestamps for import context
-    ifc_import_context.creation_date = ifc_import_context.get_creation_date()
-    ifc_import_context.iso_timestamp = ifc_import_context.get_iso_timestamp()
+class IfcCovering(Component):
+    pass
 
-    # Compare timestamps
-    if ifc_original_context.creation_date != ifc_import_context.creation_date:
-        ifc_import_context.report['creation_date'] = {
-            'expected': ifc_original_context.creation_date, 
-            'observed': ifc_import_context.creation_date
-        }
+class IfcDistribution_Element(Component):
+    pass
 
-    if ifc_original_context.iso_timestamp != ifc_import_context.iso_timestamp:
-        ifc_import_context.report['iso_timestamp'] = {
-            'expected': ifc_original_context.iso_timestamp, 
-            'observed': ifc_import_context.iso_timestamp
-        }
+class IfcDoor(Component):
+    pass
 
+class IfcMember(Component):
+        pass
 
-def compare_property_sets(ifc_original_context: IfcOriginalContext, ifc_import_context: IfcImportContext) -> None:
-    """
-    Compare property sets of entities between original and import contexts.
-    Modifies ifc_import_context.report with discrepancies in property sets.
-    """
-    for guid, original_props in ifc_original_context.entities_with_props.items():
-        import_props = ifc_import_context.entities_with_props.get(guid)
+class IfcObject(Component):
+        pass
 
-        if not import_props:
-            ifc_import_context.report[guid] = {
-                'expected_props': original_props,
-                'observed_props': 'None'
-            }
-        elif original_props != import_props:
-            ifc_import_context.report[guid] = {
-                'expected_props': original_props,
-                'observed_props': import_props
-            }
+class IfcOpening(Component):
+        pass
 
-def run():
-    original_path = '/Users/geerthesselink/Documents/BSI/Import-Certification/original_Schependomlaan (1).ifc'
-    import_path = '/Users/geerthesselink/Documents/BSI/Import-Certification/import_Schependomlaan.ifc'
+class IfcPipe(Component):
+        pass
 
-    ifc_original_context = IfcOriginalContext(original_path)
-    ifc_import_context = IfcImportContext(import_path, ifc_original_context)
+class IfcRailing(Component):
+        pass
 
-    compare_entity_counts(ifc_original_context, ifc_import_context)
-    compare_entity_guids(ifc_original_context, ifc_import_context)
-    compare_entity_timestamps(ifc_original_context, ifc_import_context)
-    compare_property_sets(ifc_original_context, ifc_import_context)
+class IfcRoof(Component):
+        pass
+
+class IfcSlab(Component):
+        pass
+
+class IfcSpace(Component):
+        pass
+
+class Ifctair(Component):
+        pass
+
+class IfcWall(Component):
+        pass
+
+class IfcWindow(Component):
+        pass
 
 
-if __name__ == '__main__':
-    run()
+class ComparisonReport():
+    def __init__(self):
+          self.discrepancies = []
+        
+    def add_discrepancy(self, discrepancy):
+        self.discrepancies.append(discrepancy)
+    
+    def display(self):
+         for discrepancy in self.discrepancies:
+            print(discrepancy)
+
+original_file = ifcopenshell.open('original_Schependomlaan (1).ifc')
+original_tree = Site(original_file.by_type('IfcSite')[0], 'Site')
+
+
+import_file = ifcopenshell.open('import_Schependomlaan.ifc')
+import_tree = Site(import_file.by_type('IfcSite')[0], 'Site')
+
+
+def generate_import_validation(original_tree, import_tree):
+    report = ComparisonReport()
+    import_projects = import_file.by_type("IfcProject")
+    if len(import_projects) > 1:
+        discrepancies = [f"project {project} is incorrectly added and no decomposition" 
+                        for project in import_projects if not project.IsDecomposedBy]
+        for discrepancy in discrepancies:
+            report.add_discrepancy(discrepancy)
+    original_tree.check_import(import_tree, report)
+
+    return report
+
+report = generate_import_validation(original_tree, import_tree)
+import pdb; pdb.set_trace()
