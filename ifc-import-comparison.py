96b7bfb22edafb3eb00f6edc35e11114e5aa84b4
Ghesselink <geert.hess@gmail.com>
2023-12-27 11:49:24 +0100

    update validation algorithm, add test files

--- ifc-import-comparison.py
+++ ifc-import-comparison.py
@@ -14,6 +14,7 @@ class Component(ABC):
         self.parent = parent
         self.entity_name = self.calculate_entity_name()
         self.guid = self.calculate_guid()
+        self.entity_type = self.calculate_type()
         
     def calculate_entity_name(self):
         # Logic to extract name from entity
@@ -22,6 +23,9 @@ class Component(ABC):
     def calculate_guid(self):
         # Logic to extract GUID from entity
         return self.entity_instance.GlobalId if hasattr(self.entity_instance, 'GlobalId') else None
+
+    def calculate_type(self):
+        return self.entity_instance.is_a()
     
     def get_parent(self):
         return self.parent
@@ -31,34 +35,56 @@ class Component(ABC):
     
     def check_import(self, other, report):
         if self.guid != other.guid:
-            report.add_discrepancy(error = 'different GUID', 
-                                   in_original = self.guid, 
-                                   in_import = other.guid)
+            report.add_deletion(self.guid, self.entity_type)
+            report.add_addition(other.guid, self.entity_type)
+            return
+
         if self.name != other.name:
-            report.add_discrepancy(error = 'different name', 
-                        in_original = self.name, 
-                        in_import = other.name)
+            report.add_deletion(self.guid, self.entity_type)
+            report.add_addition(self.guid, self.entity_type)
+
 
 class Project(Component):
-    def __init__(self, entity_instance, name):
+    """"
+    #todo perhaps not the best to start the tree with the project
+    Preference would be to have a single tree but file can contain multiple projects
+    """
+    def __init__(self, entity_instance, name, file):
         super().__init__(entity_instance, name)
+        self.file = file
         self.sites = []
         self._init_sites()
 
     def _init_sites(self):
-            decomposes = self.entity_instance.IsDecomposedBy or []
+        for decomposes in self.entity_instance.IsDecomposedBy or []:
             for related_object in decomposes.RelatedObjects:
                 if related_object.is_a('IfcSite'):
                     self.sites.append(Site(entity_instance = related_object,
                                         name = 'Site', 
                                         parent=self))
+    
+    def check_import(self, other, report):
+        super().check_import(other, report) 
+
+
+        original_sites = {s.guid: s for s in self.sites}
+        import_sites = {s.guid: s for s in other.sites}
+
+        for guid, storey in original_sites.items():
+            if guid in import_sites:
+                storey.check_import(import_sites[guid], report)
+            else:
+                report.add_deletion(guid, self.entity_type)
+
+        for guid, storey in import_sites.items():
+            if guid not in original_sites:
+                report.add_addition(guid, self.entity_type)
 
 
 class Site(Component):
-    def __init__(self, entity_instance, name, file):
-        super().__init__(entity_instance, name, file)
+    def __init__(self, entity_instance, name, parent = None):
+        super().__init__(entity_instance, name, parent = None)
         self.buildings = []
-        self.file = file
         self._init_buildings()
 
     def _init_buildings(self):
@@ -76,22 +102,20 @@ class Site(Component):
             building.display()
         
     def check_import(self, other, report):
-        super().check_import(other, report)
-        if len(self.buildings) != len(other.buildings):
-            report.add_discrepancy(error = 'Different number of buildings in site', 
-                                    in_original = len(self.buildings),
-                                    in_import = len(other.buildings))
-        for self_building, other_building in zip(self.buildings, other.buildings):
-            if self_building.guid != other_building.guid:
-                report.add_discrepancy(error=f"Mismatch in building GUID",
-                                       in_original=self_building.guid,
-                                       in_import=other_building.guid)
-            if self_building.name != other_building.name:
-                report.add_discrepancy(error=f"Mismatch in building name",
-                                       in_original=self_building.name,
-                                       in_import=other_building.name)
+        super().check_import(other, report) 
 
-            self_building.check_import(other_building, report)
+        original_buildings = {s.guid: s for s in self.buildings}
+        import_buildings = {s.guid: s for s in other.buildings}
+
+        for guid, storey in original_buildings.items():
+            if guid in import_buildings:
+                storey.check_import(import_buildings[guid], report)
+            else:
+                report.add_deletion(guid, original_buildings[guid].entity_type)
+
+        for guid, storey in import_buildings.items():
+            if guid not in original_buildings:
+                report.add_addition(guid, import_buildings[guid].entity_type)
 
 
 class Building(Component):
@@ -107,6 +131,7 @@ class Building(Component):
                 if related_object.is_a('IfcBuildingStorey'):
                     self.storeys.append(Storey(entity_instance = related_object,
                                                name = related_object.Name, parent=self))
+        
 
     def display(self):
         print(f'Building: {self.entity_instance} ({self.guid}')
@@ -116,25 +141,22 @@ class Building(Component):
     def get_storey_names(self):
         return [storey.name for storey in self.storeys]
 
+
     def check_import(self, other, report):
-        super().check_import(other, report)
-        if len(self.storeys) != len(other.storeys):
-            report.add_discrepancy(error='Different number of storeys in building',
-                                   in_original=len(self.storeys),
-                                   in_import=len(other.storeys))
-            return  # Skip further comparison if storey count differs
+        super().check_import(other, report) 
 
-        for self_storey, other_storey in zip(self.storeys, other.storeys):
-            if self_storey.guid != other_storey.guid:
-                report.add_discrepancy(error=f"Mismatch in storey GUID",
-                                       in_original=self_storey.guid,
-                                       in_import=other_storey.guid)
-            if self_storey.name != other_storey.name:
-                report.add_discrepancy(error=f"Mismatch in storey name",
-                                       in_original=self_storey.name,
-                                       in_import=other_storey.name)
+        original_storeys = {s.guid: s for s in self.storeys}
+        import_storeys = {s.guid: s for s in other.storeys}
 
-            self_storey.check_import(other_storey, report)
+        for guid, storey in original_storeys.items():
+            if guid in import_storeys:
+                storey.check_import(import_storeys[guid], report)
+            else:
+                report.add_deletion(guid, original_storeys[guid].entity_type)
+
+        for guid, storey in import_storeys.items():
+            if guid not in original_storeys:
+                report.add_addition(guid, import_storeys[guid].entity_type)
 
 
 class Storey(Component):
@@ -159,12 +181,15 @@ class Storey(Component):
             'IfcWindow': []
         }
         self._init_storey_elements()
+        pass
 
     def _init_storey_elements(self):
         contains_elements = self.entity_instance.ContainsElements or []
         for contains_element in contains_elements:
             for related_element in contains_element.RelatedElements:
                 entity_type = related_element.is_a()
+                if entity_type == 'IfcStair':
+                    pass
                 component_class = globals().get(entity_type, None)
                 if component_class and entity_type in self.components:
                     component = component_class(entity_instance=related_element,
@@ -172,19 +197,28 @@ class Storey(Component):
                                                 parent=self)
                     self.components[entity_type].append(component)
 
+
     def display(self):
         print(f"Storey: {self.entity_instance} ({self.guid}")
 
     def check_import(self, other, report):
         super().check_import(other, report)
-        for component_type in self.components:
-            if len(self.components[component_type]) != len(other.components[component_type]):
-                report.add_discrepancy(error=f'Different number of {component_type} in storey {self.name}',
-                                       in_original=len(self.components[component_type]),
-                                       in_import=len(other.components[component_type]))
-            else:
-                for self_component, other_component in zip(self.components[component_type], other.components[component_type]):
-                    self_component.check_import(other_component, report)
+        for comp_type, orig_components in self.components.items():
+            import_components = other.components.get(comp_type, [])
+
+            orig_guid_map = {comp.guid: comp for comp in orig_components}
+            import_guid_map = {comp.guid: comp for comp in import_components}
+            for guid, comp in orig_guid_map.items():
+                if guid not in import_guid_map:
+                    report.add_deletion(guid, comp_type)
+
+            for guid, comp in import_guid_map.items():
+                if guid not in orig_guid_map:
+                    report.add_addition(guid, comp_type)
+
+                elif orig_guid_map[guid].name != comp.name:
+                    report.add_deletion(guid, comp_type)
+                    report.add_addition(guid, comp_type)
 
 class IfcBeam(Component):
     pass
@@ -225,7 +259,7 @@ class IfcSlab(Component):
 class IfcSpace(Component):
         pass
 
-class Ifctair(Component):
+class IfcStair(Component):
         pass
 
 class IfcWall(Component):
@@ -237,37 +271,24 @@ class IfcWindow(Component):
 
 class ComparisonReport:
     def __init__(self):
-        self.discrepancies = []
+        self.additions = []
+        self.deletions = []
 
-    def add_discrepancy(self, error = 'error', in_original = None, in_import = None):
-        self.discrepancies.append({'error': error, 'in_original': in_original, 'in_import': in_import})
+    def add_addition(self, guid, entity_type):
+        self.additions.append({'guid' : guid, 'entity_type': entity_type})
+
+    def add_deletion(self, guid, entity_type):
+        self.deletions.append({'guid' : guid, 'entity_type': entity_type})
+
+    def add_modification(self, guid, entity_type):
+        self.add_deletion({'guid' : guid, 'entity_type': entity_type})
+        self.add_addition({'guid' : guid, 'entity_type': entity_type})
 
     def display(self):
-        for discrepancy in self.discrepancies:
-            print(f"Error: {discrepancy['error']}, in_original: {discrepancy['in_original']}, in_import: {discrepancy['in_import']}")
-    
-    def to_dataframe(self):
-        return pd.DataFrame(self.discrepancies)
-    
-    def export_to_csv(self, filename):
-        df = self.to_dataframe()
-        df.to_csv(filename, index=False)
-    
-    def error_summary(self):
-        return self.to_dataframe().error.value_counts()
-
-
-def generate_import_validation(original_tree, import_tree):
-    report = ComparisonReport()
-    import_projects = import_tree.file.by_type("IfcProject")
-    if len(import_projects) > 1:
-        discrepancies = [f"project {project} is incorrectly added and no decomposition" 
-                        for project in import_projects if not project.IsDecomposedBy]
-        for discrepancy in discrepancies:
-            report.add_discrepancy(error = 'Incorrect decomposition IfcProject', in_original = 'Only single IfcProject', in_import = discrepancy)
-    original_tree.check_import(import_tree, report)
-
-    return report
+        for i in self.deletions:
+            print(f"\033[91mDeleted: {i['guid']} ({i['entity_type']})\033[0m")  # Red text for deletions
+        for i in self.additions:
+            print(f"\033[92mAdded: {i['guid']} ({i['entity_type']})\033[0m")    # Green text for additions
 
 
 
@@ -280,18 +301,19 @@ def get_property_set(entity: IfcEntity) -> Optional[Any]:
                     return property_set
         return None
 
-def run(original_fn, import_fn):
+
+def run(original_fn : str, import_fn : str):
+    report = ComparisonReport()
+
     original_file = ifcopenshell.open(original_fn)
-    original_tree = Site(original_file.by_type('IfcSite')[0], 'Site', original_file)
+    original_tree = Project(original_file.by_type('IfcProject')[0], 'Project', original_file)
 
     import_file = ifcopenshell.open(import_fn)
-    import_tree = Site(import_file.by_type('IfcSite')[0], 'Site', file = import_file)
-    report = generate_import_validation(original_tree, import_tree)
+    import_tree = Project(import_file.by_type('IfcProject')[0], 'Project', file = import_file)
 
-    report.export_to_csv('report.csv')
+    original_tree.check_import(import_tree, report)
 
-    error_summary = report.error_summary()
-    print(error_summary)
+    report.display()
 
 if __name__ == '__main__':
-    run(original_fn = 'original_Schependomlaan (1).ifc', import_fn = 'import_Schependomlaan.ifc')
+    run(original_fn = 'original_Schependomlaan (1).ifc', import_fn = 'test_files/1_schependomlaan.ifc')
